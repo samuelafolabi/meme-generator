8570c526501036cb264df6ee248cc01fb0502034
Samuel Afolabi <folaborn2013@gmail.com>
2022-04-08 00:22:56 +0100

    Meme generator react practice project 1

---
+++ src/components/Meme.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemes, setAllMemes] = useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMemeImage() {
+    // const allMemes = allMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({ ...prevMeme, [name]: value }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form__input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form__input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+
+        <button className="form__button" onClick={getMemeImage}>
+          Get a new meme image 😎
+        </button>
+      </div>
+
+      <div className="meme">
+        <img
+          src={meme.randomImage}
+          className="meme__img"
+          alt="this_is_the_meme"
+        />
+        <h2 className="meme__text top">{meme.topText}</h2>
+        <h2 className="meme__text bottom">{meme.bottomText}</h2>
+      </div>
+    </main>
+  );
+}
+
+export default Meme;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <nav className="nav">
+      <img src="../images/pepethefrog.png" className="nav__logo" />
+      <h2 className="nav__title">Meme Generator</h2>
+      <h4 className="nav__subtitle">Built with react.js</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
